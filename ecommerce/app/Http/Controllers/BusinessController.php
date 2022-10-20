12b15f0c8fc69b53a5f0443fa6d2e29378349faa
Victor Martins <vitorchiesa74@gmail.com>
2022-10-20 15:45:49 -0300

    Introduction to Eloquent - Updating Records

--- ecommerce/app/Http/Controllers/BusinessController.php
+++ ecommerce/app/Http/Controllers/BusinessController.php
@@ -9,11 +9,27 @@ class BusinessController extends Controller
 {
     public function index()
     {
-        $businessAll = Business::create([
-            'name' => 'Victor Martins',
-            'email' => 'victor@gmail.com',
-            'address' => 'Rua a Quadra b',
-        ]);
+        // $businessAll = Business::find(1)->update([
+        //     'name' => 'Victor',
+        //     'email' => 'victor@gmail.com',
+        //     'address' => 'rua dos doidos'
+        // ]);
+
+        // $businessAll = Business::find(1);
+        // $businessAll->name = 'Tiago';
+        // $businessAll->email = 'tiago@laravel9.com';
+        // $businessAll->address = 'Quadra c rua B';
+        // $businessAll->save();
+
+        $input = [
+            'name' => 'Victor2',
+            'email' => 'victor2@gmail.com',
+            'address' => 'rua dos doidos2'
+        ];
+
+        $businessAll = Business::find(1);
+        $businessAll->fill($input);
+        $businessAll->save();
 
         dd($businessAll);
     }
